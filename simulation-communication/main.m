b7ef5d5f6851795d0a22fb7aebfd9eac90dc8e7d
Victor Lindeman <40773809+vlindeman@users.noreply.github.com>
2019-03-25 11:48:58 +0100

    Add files via upload

--- simulation-communication/main.m
+++ simulation-communication/main.m
@@ -1,37 +1,79 @@
-%% STANFORD UNIVERSITY INTERIM model, NO-LOS 
+
 % Radio Propagation Path Loss Models for5G Cellular Networks in the 28 GHz and38 GHz Millimeter-Wave Bands
 % https://ieeexplore-ieee-org.e.bibl.liu.se/stamp/stamp.jsp?tp=&arnumber=6894456
- 
-% Suburban enviroment, single narrow beam  
-% PLSUI,Mod[dB] (d) = aNLOS × (PLSUI(d) –PLSUI(d0)) + PL(d0) + Xs
+
+%% Modified SUI - 28 GHz NLOS directional path loss 
+% PLE: right NLOS in Table 1 
 clear 
-clc 
+ 
+d = 60;
 
-d = 200;
-d0 = 100;
-f = 2*10^9;
-hb = 40;
-hr = 10;
-s = 10; 
-n = 1;
+f = 28*10^9;
+lambda = physconst('LightSpeed')/f;
+
+% Height of TX and RX (m) 
+hb = 17;
+hr = 1.5;
+
+% Found PLE for omni 
+gamma = 4.5;
+sigma = 10.8;
+%s = normrnd(0,sigma)
+s = sigma;
+d0 = 1; 
+TXgain = 24.5;
+RXgain = 24.5;
+
+% Correction factor 
+alpha = 0.71;
+
+% PLsui(d)
+A = 20*log10((4*pi*d0)/lambda);
+Xfc = 6*log10((f/10^6)/2000);
+Xrx = -10.8*log10(hr/2);
+PL_sui_d = A + 10*gamma*log10(d/d0)+s+Xfc+Xrx;  
+
+% PLsui(d0)
+PL_sui_d0 = A + 10*gamma*log10(d0/d0)+s+Xfc+Xrx;  
 
 
-% [PL] = PL_SUIbasic(d, d0,freq,hb,s,n)
-PL = PL_SUIbasic(d, d0, f, hb, s, n)
-
-% [PL] = PL_SUI(d, d0,freq,hb,hr,s,n)
-PL2 = PL_SUI(d, d0, f, hb, hr, s, n)
-
-% PL = PL-SUI-MOD(alpha, d, d0,freq,hb,hr,s,n)
-PL3 = PL_SUI_MOD(1, d, d0, f, hb, hr, s, n)
-
-pt = 10
-
-q = PL/10; 
-pr = pt/(10^q)
-
-%% STANFORD UNIVERSITY INTERIM model, LOS
-% Suburban enviroment, single narrow beam 
-% PLFS,Mod[dB](d) = aLOS ×(PLFS(d) –PLFS(d0)) + PL(d0) + Xs
+PL = alpha*(PL_sui_d - PL_sui_d0) + A + s  
+PL_gain = PL - TXgain - RXgain
 
 
+%% Modified SUI - 28 GHz NLOS omni path loss 
+clear 
+ 
+d = 60;
+
+f = 28*10^9;
+lambda = physconst('LightSpeed')/f;
+
+% Height of TX and RX (m) 
+hb = 17;
+hr = 1.5;
+
+% Found PLE for omni 
+gamma = 3.4; % "n" in report 
+sigma = 9.7;
+%s = normrnd(0,sigma)
+s = sigma;
+d0 = 1; 
+TXgain = 24.5;
+RXgain = 10.9;
+
+% Correction factor 
+alpha = 0.71;
+
+% PLsui(d)
+A = 20*log10((4*pi*d0)/lambda);
+Xfc = 6*log10((f/10^6)/2000);
+Xrx = -10.8*log10(hr/2);
+PL_sui_d = A + 10*gamma*log10(d/d0)+s+Xfc+Xrx;  
+
+% PLsui(d0)
+PL_sui_d0 = A + 10*gamma*log10(d0/d0)+s+Xfc+Xrx;  
+
+
+PL = alpha*(PL_sui_d - PL_sui_d0) + A + s  
+PL_gain = PL - TXgain - RXgain
