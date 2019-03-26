25611eafea3fec53847ce3030024abe02f5b2d71
Victor Lindeman <v.lindeman@protonmail.com>
2019-03-26 13:28:33 +0100

    nyc model

---
+++ simulation-communication/main_citymodel.m
@@ -0,0 +1,7 @@
+% From the report: 
+% Omnidirectional Path Loss Models in New YorkCity at 28 GHz and 73 GHz
+% https://ieeexplore-ieee-org.e.bibl.liu.se/stamp/stamp.jsp?tp=&arnumber=7136165
+
+%%
+
+
