94b78786533661e3c39298e589f25554fbd87ea0
Victor Lindeman <v.lindeman@protonmail.com>
2019-04-25 13:21:55 +0200

    changed neg->pos for linprog

--- simulation-communication/pathloss-models/power_opt.m
+++ simulation-communication/pathloss-models/power_opt.m
@@ -9,7 +9,7 @@ A = [-1]; b = [(PL+PrMin)];
 
 % objective function 
 f = [1];
-x = linprog(-f,-A,-b);
+x = linprog(f,A,b);
 x = -x % convert, due to matlab default form
 
 db2pow(x) % Power in Watt 
