b9a0ac6e32d24d606d1cef8620833423f14b839d
Victor Lindeman <v.lindeman@protonmail.com>
2019-05-16 09:06:39 +0200

    update

--- simulation-communication/pathloss-models/shannon_cap_SINR.m
+++ simulation-communication/pathloss-models/shannon_cap_SINR.m
@@ -1,10 +1,11 @@
-%% Calculate Shannon cap with given PL  
+%% Calculate SINR with given PL matrix  
 clc
 
 % enter Pt and Bandwidth 
 PtWatt = 48.3209
 B = 1500 * 10^6;
 
+Pi = 1
 
 PtdB   = pow2db(PtWatt);
 N0 = 10^-9;
@@ -12,22 +13,17 @@ N0 = 10^-9;
 % calculate and convert Pr
 Pr_dB = PtdB  - PL;
 Pr = db2pow(Pr_dB);
+Pr = Pr(91)
 
 % calculate SNR 
-SNR = Pr/(N0*B);
+SINR = Pr/(N0*B + Pi);
 
 % calculate Shannon capacity 
-C = B*log2(1+SNR);
-
-% print values 
-C(41)
-C(91)
-C(191)
-min(C)
+C = B*log2(1+SINR);
 
 plot(d,C)
 hold on 
 axis([0 inf 0 inf])
 xlabel('Distance [m]') 
 ylabel('Capacity [bps] ')  
- 
+
